b9e3417f686941e7f89a5e84ab00bf4ba76f0527
Low Tee <lowtee.vn@gmail.com>
2024-12-14 08:25:22 +0700

    vault backup: 2024-12-14 08:25:22

---
+++ outlinecreate.py
@@ -0,0 +1,29 @@
+import os
+
+def scan_directory(directory, prefix=''):
+    structure = ''
+    for item in os.listdir(directory):
+        if item.startswith('.') or item.startswith('_'):
+            continue
+        path = os.path.join(directory, item)
+        if os.path.isdir(path):
+            structure += f'{prefix}{item}\n'
+            structure += scan_directory(path, prefix + '    ')
+        else:
+            relative_path = os.path.relpath(path)
+            file_url = f'{relative_path}'
+            structure += f'{prefix}[[{file_url}]] \n'
+    return structure
+
+
+def save_structure_to_readme(directory, output_file='README.md'):
+    structure = scan_directory(directory)
+    with open(output_file, 'w', encoding='utf-8') as file:
+        file.write('# Project Structure\n\n')
+        file.write(structure)
+    print(f'Cấu trúc thư mục đã được lưu vào {output_file}')
+
+
+if __name__ == '__main__':
+    project_path = os.getcwd()  # Quét thư mục hiện tại
+    save_structure_to_readme(project_path)
